c03137caa5060ac01bcc4ba59155870d7555787b
erick patrick <github@erickpatrick.net>
2023-08-21 00:58:56 +0200

    adds routes to deal with weekly
    - needs proper redirect for new "rpg-na-semana.com"
    - needs proper styling

---
+++ resources/views/pages/weekly/anuncie-na-weekly.blade.php
@@ -0,0 +1,18 @@
+@php
+    use App\Models\Page;
+
+    $page = Page::whereNot('status', 'draft')
+        ->whereSlug('anuncie-na-weekly')
+        ->get()->pop();
+
+        abort_unless($page, 404);
+@endphp
+
+<x-layout
+    menu="/artigos"
+    title="{{ $page->title }}"
+    description="{{ $page->description }}">
+    <h2>{{ $page->title }}</h2>
+
+    <x-buk-markdown>{!! $page->content !!}</x-buk-markdown>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Spatie\Sitemap\SitemapGenerator;
 
+Route::get('/weekly/{week}', function (string $week) {
+    return "redirecionar para rpg-na-semana.com/" . $week;
+})->where('week', '[0-9]+');
+
 Route::feeds();
 
 Route::get('generate-sitemap-site-pessoal', function() {
