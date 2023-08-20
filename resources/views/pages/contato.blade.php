4a36d9a2204c84bba11cd50ed795600da52db102
erick patrick <github@erickpatrick.net>
2023-08-20 19:22:52 +0200

    adds basic view of static pages

---
+++ resources/views/pages/contato.blade.php
@@ -0,0 +1,11 @@
+<x-layout menu="/contato" title="Contato" description="Caso queira contatar-me, aqui estão algumas das formas mais rápidas que você pode usar para entrar em contato comigo a qualquer momento">
+    <x-container>
+        <div class="py-8 sm:py-16 lg:py-24 max-w-xl mx-auto">
+            <div class="py-4">
+                <h4 class="text-xl font-medium leading-6 text-gray-900">Bom e velho email de contato</h4>
+            </div>
+
+            <livewire:contact />
+        </div>
+    </x-container>
+</x-layout>
