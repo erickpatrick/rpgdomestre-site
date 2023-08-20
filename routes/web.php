730398ed9034bd30dc0ccf870330fe0e11c6659d
erick patrick <github@erickpatrick.net>
2023-08-20 16:52:07 +0200

    adds toutes to feeds and sitemap

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\Sitemap\SitemapGenerator;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::feeds();
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('generate-sitemap-site-pessoal', function() {
+    SitemapGenerator::create('https://rpgdomestre.com')
+    ->getSitemap()
+    ->writeToFile(public_path('sitemap.xml'));
 });
